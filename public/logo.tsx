9e7824a3f47974f857cb20c68e78ff03c53e2f56
MaxBrych <max.brych03@gmail.com>
2024-02-02 23:00:27 +0100

    initial commit

---
+++ public/logo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="40" height="40" fill="black" />
+      <rect x="16.6602" y="9.87354" width="6.75" height="6.75" fill="white" />
+      <rect x="23.4453" y="16.625" width="6.67969" height="13.5" fill="white" />
+      <rect x="9.91016" y="9.87354" width="6.75" height="20.25" fill="white" />
+    </svg>
+  );
+}
